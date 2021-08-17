a3e657eeda43148e015a61678fa487fc30f08cfa
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-08-17 12:48:30 -0500

    2 checkout done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [token, setToken] = useState('');
+
+    const getParamsFromHash = (hash) => {
+        const hashContent = hash.substr(1); // removes #
+        const paramsSplit = hashContent.split('&'); // returns list with keys and values
+        let params = {};
+        let values = [];
+        paramsSplit.forEach((item) => {
+            values = item.split('=');
+            params[values[0]] = values[1];
+        });
+        return params;
+    };
+    useEffect(() => {
+        setToken(localStorage.getItem('token'));
+    }, [token]);
+        
+
+    useEffect(() => {
+        if(window.location.hash || token) {
+            const hash = window.location.hash;
+            const tokens = getParamsFromHash(hash);
+            setToken(tokens.token);
+            localStorage.setItem('access_token', tokens.access_token);
+            window.history.pushState({}, null, '/home');
+        }
+    }, []);
 
     return (
         <div>
-            Hello from Home!
+            {token && <div>
+                <h1>Home</h1>
+                <p>Here's the token:</p>
+                <p>{token}</p>
+                </div>
+            }
         </div>
     );
 };
