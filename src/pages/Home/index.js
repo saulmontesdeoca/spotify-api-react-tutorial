4980bf204a483ca4f0607f233b545a0af50f9cd5
Saul Montes De Oca <mr.sauce97@gmail.com>
2021-08-17 14:46:15 -0500

    3 axios checkpoint

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
     const [token, setToken] = useState('');
+    const [playlists, setPlaylists] = useState({});
+    const [artists, setArtists] = useState({});
+    const [profile, setProfile] = useState({});
+
+    const PLAYLISTS_ENDPOINT = "https://api.spotify.com/v1/me/playlists";
+    const TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks?time_range=long_term";
+    const ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists?time_range=long_term";
+    const PROFILE_ENDPOINT = "https://api.spotify.com/v1/me";
 
     const getParamsFromHash = (hash) => {
         const hashContent = hash.substr(1); // removes #
@@ -14,19 +23,42 @@ const Home = () => {
         });
         return params;
     };
-    useEffect(() => {
+
+    // function to request data from spotify api
+    const getData = async (endpoint, setFunction) => {
+        await axios.get(endpoint, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}` // pass token to header
+            },
+        }).then(response => {
+            setFunction(response.data);
+            console.log(response.data);
+        }).catch(error => {
+            console.log(error);
+        });
+
+    }
+     useEffect(() => {
         setToken(localStorage.getItem('token'));
     }, [token]);
         
 
-    useEffect(() => {
+    useEffect( () => { 
+        // getting the token from the hash
         if(window.location.hash || token) {
             const hash = window.location.hash;
             const tokens = getParamsFromHash(hash);
-            setToken(tokens.token);
-            localStorage.setItem('access_token', tokens.access_token);
+            if(!token) {
+                setToken(tokens.token);
+            }
+            localStorage.setItem('token', tokens.access_token);
             window.history.pushState({}, null, '/home');
         }
+        // request to spotify api to fetch 
+        getData(PLAYLISTS_ENDPOINT, setPlaylists);
+        getData(TRACKS_ENDPOINT, setArtists);
+        getData(ARTISTS_ENDPOINT, setProfile);
+        getData(PROFILE_ENDPOINT, setProfile);
     }, []);
 
     return (
